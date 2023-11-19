1da472c277e8f6ae64d1ae1e38d9e0ac09469130
haveyouheardofthehighelves <32472487+haveyouheardofthehighelves@users.noreply.github.com>
2023-11-19 00:35:49 -0800

    attempting to make this better at rescaling

--- PythonFiles/UITesting.py
+++ PythonFiles/UITesting.py
@@ -3,10 +3,8 @@ import cv2
 import os
 from PIL import ImageTk, Image
 
-
 def close_window():
     master.destroy()
-
 
 current_directory = os.getcwd()
 os.chdir('../')
@@ -20,14 +18,12 @@ print(xml_file_path)
 face_cascade = cv2.CascadeClassifier(xml_file_path)
 cap = cv2.VideoCapture(0)
 
-
 def scanbody(part, B, G, R, frame):
     for (x, y, w, h) in part:
         center_x = x + w // 2
         center_y = y + h // 2
         pos = (center_x, center_y)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (B, G, R), 3)
-
 
 def camdisplay():
     _, frame = cap.read()
@@ -46,23 +42,23 @@ def camdisplay():
     VideoLabel.imgtk = imgtk
     VideoLabel.configure(image=imgtk)
     VideoLabel.after(20, camdisplay)
+    angle = 90
+    Angle_Update.config(text=f"Servo Angle: {angle}")
 
+    # Update the angle label (example, you can replace it with your actual angle calculation)
 
 master = tk.Tk()
 master.title('Hello')
-master.geometry("1400x600")  # Change the dimensions as needed
-master.resizable(width=0, height=0)
 
-# Create a grid for the master window
-master.grid_rowconfigure(0, weight=5)
-master.grid_columnconfigure(0, weight=5)
+# Configure row 0 to expand with a weight of 1
+master.grid_rowconfigure(0, weight=1)
 
+# Configure column 0 to expand with a weight of 1
+master.grid_columnconfigure(0, weight=1)
+
+# Create a single canvas
 C_1 = tk.Canvas(master)
 C_1.grid(row=0, column=0, sticky="nsew")
-C_2 = tk.Canvas(master)
-C_2.grid(row=0, column=2, sticky="nsew")
-C_2 = tk.Canvas(master)
-C_2.grid(row=0, column=2, sticky="nsew")
 
 # Create labels inside the canvas
 label_width = 30
@@ -71,31 +67,33 @@ AI_sens = tk.Scale(C_1, from_=0, to=180, orient="horizontal", length=300)
 
 AI_Label = tk.Label(C_1, text='AI', width=label_width, height=label_height, borderwidth=2, relief="solid",
                     font=("Georgia", 10))
-Manual_Label = tk.Label(C_2, text='Manual', width=label_width, height=label_height, borderwidth=2, relief="solid",
+
+Manual_Label = tk.Label(C_1, text='Manual', width=label_width, height=label_height, borderwidth=2, relief="solid",
                         font=("Georgia", 10))
 
 # Create Scale widget for AI_sens
 AI_sens.set(90)
 
 # Create VideoLabel
-VideoLabel = tk.Label(C_2, borderwidth=3, relief="solid")
-VideoLabel.grid(row=0, column=0, padx=50, pady=50, sticky="ne")
+VideoLabel = tk.Label(C_1, borderwidth=3, relief="solid")
+VideoLabel.grid(row=0, column=1, padx=50, pady=50, sticky="ne")
 
 # Create another Scale widget for Manual_Label
-Manual_Sens = tk.Scale(master, from_=0, to=180, orient="horizontal", length=300)
+Manual_Sens = tk.Scale(C_1, from_=0, to=180, orient="horizontal", length=300)
 Manual_Sens.set(90)
 
-Title_Label = tk.Label(C_2, text="AI Feed", font=("Georgia", 16))
-Angle_Update = tk.Label(C_2, text="Servo Angle: 90", font=("Georgia", 16))
-# Create other labels and Scale using grid
-AI_Label.grid(row=0, column=1, padx=50, pady=185, sticky="e")  # Adjust padx and sticky for AI_Label
-Manual_Label.grid(row=0, column=2, padx=50, pady=185, sticky="e")  # Adjust padx and sticky for Manual_Label
-AI_sens.grid(row=0, column=1, sticky="nw", padx=15, pady=120, columnspan=2)
+Title_Label = tk.Label(C_1, text="AI Feed", font=("Georgia", 16))
+Angle_Update = tk.Label(C_1, text="Servo Angle: 90", font=("Georgia", 16))
 
-Manual_Sens.grid(row=0, column=2, padx=10, pady=120, sticky="ne", columnspan=2)
+# Create other labels and Scale using grid
+AI_Label.grid(row=0, column=2, padx=50, pady=185, sticky="e")  # Adjust padx and sticky for AI_Label
+Manual_Label.grid(row=0, column=3, padx=50, pady=185, sticky="e")  # Adjust padx and sticky for Manual_Label
+AI_sens.grid(row=0, column=2, sticky="nw", padx=15, pady=120, columnspan=2)
+
+Manual_Sens.grid(row=0, column=3, padx=10, pady=120, sticky="ne", columnspan=2)
 
 Title_Label.grid(row=0, column=0, sticky='n', columnspan=2, pady=10)
-Angle_Update.grid(row=0, column=0, sticky='s', columnspan=2, pady = 100)
+Angle_Update.grid(row=0, column=0, sticky='s', columnspan=2, pady=100)
 
 camdisplay()
 master.mainloop()
